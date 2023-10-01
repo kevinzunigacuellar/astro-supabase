33cad26ad81068f987ab627aef8371480d296a3c
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2023-10-01 14:48:02 -0400

    add basic auth with supabase

---
+++ src/pages/api/auth/register.ts
@@ -0,0 +1,23 @@
+import type { APIRoute } from "astro";
+import { supabase } from "../../../scripts/supabase";
+
+export const POST: APIRoute = async ({ request, redirect }) => {
+  const formData = await request.formData();
+  const email = formData.get("email")?.toString();
+  const password = formData.get("password")?.toString();
+
+  if (!email || !password) {
+    return new Response("Email and password are required", { status: 400 });
+  }
+
+  const { error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
+
+  if (error) {
+    return new Response(error.message, { status: 500 });
+  }
+
+  return redirect("/signin");
+};
---
+++ src/pages/api/auth/signin.ts
@@ -0,0 +1,26 @@
+import type { APIRoute } from "astro";
+import { supabase } from "../../../scripts/supabase";
+
+export const POST: APIRoute = async ({ request, cookies, redirect }) => {
+  const formData = await request.formData();
+  const email = formData.get("email")?.toString();
+  const password = formData.get("password")?.toString();
+
+  if (!email || !password) {
+    return new Response("Email and password are required", { status: 400 });
+  }
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) {
+    return new Response(error.message, { status: 500 });
+  }
+
+  const { access_token, refresh_token } = data.session;
+  cookies.set("sb-access-token", access_token, { path: "/" });
+  cookies.set("sb-refresh-token", refresh_token, { path: "/" });
+  return redirect("/dashboard");
+};
