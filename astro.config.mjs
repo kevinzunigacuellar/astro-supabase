2a1cb9be127d87991199e5073c467268b8226c6d
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2023-10-05 17:22:31 -0400

    disable vercel middleware

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel({
-    edgeMiddleware: true,
-  })
+  adapter: vercel()
 });
