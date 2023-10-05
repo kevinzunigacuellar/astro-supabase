b4fcf8a6c9cc8db4add33561c811b1876dde7e30
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2023-10-05 17:11:33 -0400

    add vercel ssr adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  adapter: vercel()
 });
