b0e4b8fb2038627721f281c537d425e19e54a747
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2023-10-05 16:58:42 -0400

    move auth check to middleware

---
+++ src/middleware/index.ts
@@ -0,0 +1,50 @@
+import { defineMiddleware } from "astro:middleware";
+import { supabase } from "../scripts/supabase";
+import { cookieOptions } from "../scripts/utils";
+
+const protectedRoutes = ["/dashboard"];
+const redirectRoutes = ["/signin", "/register"];
+
+export const onRequest = defineMiddleware(
+  async ({ locals, url, cookies, redirect }, next) => {
+    if (protectedRoutes.includes(url.pathname)) {
+      const accessToken = cookies.get("sb-access-token");
+      const refreshToken = cookies.get("sb-refresh-token");
+
+      if (!accessToken || !refreshToken) {
+        return redirect("/signin");
+      }
+
+      const { data, error } = await supabase.auth.setSession({
+        refresh_token: refreshToken.value,
+        access_token: accessToken.value,
+      });
+
+      if (error) {
+        return redirect("/signin");
+      }
+
+      locals.email = data.user?.email!;
+      cookies.set(
+        "sb-access-token",
+        data?.session?.access_token!,
+        cookieOptions,
+      );
+      cookies.set(
+        "sb-refresh-token",
+        data?.session?.refresh_token!,
+        cookieOptions,
+      );
+    }
+
+    if (redirectRoutes.includes(url.pathname)) {
+      const accessToken = cookies.get("sb-access-token");
+      const refreshToken = cookies.get("sb-refresh-token");
+
+      if (accessToken && refreshToken) {
+        return redirect("/dashboard");
+      }
+    }
+    return next();
+  },
+);
