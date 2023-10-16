1dd58d04da1ff64cadc301728961221c46ce4429
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2023-10-16 13:58:06 -0700

    add solidjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [tailwind()],
+  integrations: [tailwind(), solidJs()]
 });
