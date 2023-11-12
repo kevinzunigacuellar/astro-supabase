1447add97b91f6d04d65df4a9a7a47fb1bb9cb28
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2023-11-12 16:14:34 -0500

    fix build and add .vercel to gitignore

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://astro-supabase-auth.vercel.app",
   output: "server",
   adapter: vercel(),
   integrations: [tailwind(), solidJs()]
