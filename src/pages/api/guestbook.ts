71b65e4817412cfbe79b5d3e50984c021d8ba297
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2024-02-10 14:27:31 -0500

    Add token verification to API routes

--- src/pages/api/guestbook.ts
+++ src/pages/api/guestbook.ts
@@ -1,7 +1,36 @@
 import type { APIRoute } from "astro";
 import { supabase } from "../../lib/supabase";
 
-export const GET: APIRoute = async () => {
+export const GET: APIRoute = async ({cookies}) => {
+  
+  const accessToken = cookies.get("sb-access-token");
+  const refreshToken = cookies.get("sb-refresh-token");
+
+  // Check for tokens
+  if (!accessToken || !refreshToken) {
+    return new Response(
+      JSON.stringify({
+        error: "Unauthorized",
+      }),
+      { status: 401 },
+    );
+  }
+
+  // Verify the tokens
+  const { error: AuthError } = await supabase.auth.setSession({
+    access_token: accessToken.value,
+    refresh_token: refreshToken.value,
+  })
+
+  if (AuthError) {
+    return new Response(
+      JSON.stringify({
+        error: "Unauthorized"
+      }),
+      { status: 401 },
+    );
+  }
+  
   const { data, error } = await supabase
     .from("guestbook")
     .select("*")
@@ -19,7 +48,36 @@ export const GET: APIRoute = async () => {
   return new Response(JSON.stringify(data));
 };
 
-export const POST: APIRoute = async ({ request }) => {
+export const POST: APIRoute = async ({ request, cookies }) => {
+
+  const accessToken = cookies.get("sb-access-token");
+  const refreshToken = cookies.get("sb-refresh-token");
+
+  // Check for tokens
+  if (!accessToken || !refreshToken) {
+    return new Response(
+      JSON.stringify({
+        error: "Unauthorized",
+      }),
+      { status: 401 },
+    );
+  }
+
+  // Verify the tokens
+  const { error: AuthError } = await supabase.auth.setSession({
+    access_token: accessToken.value,
+    refresh_token: refreshToken.value,
+  })
+
+  if (AuthError) {
+    return new Response(
+      JSON.stringify({
+        error: "Unauthorized"
+      }),
+      { status: 401 },
+    );
+  }
+  
   const { name, message } = await request.json();
   const { data, error } = await supabase
     .from("guestbook")
